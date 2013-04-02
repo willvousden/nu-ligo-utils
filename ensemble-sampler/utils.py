b4c05ce67d0bbdc7f3e7b39d3b5534f429064ac3
Will Meierjurgen Farr <wmfarr@gmail.com>
2013-04-02 08:49:10 -0400

    Moved tail to utils.py, removing tail.py

--- ensemble-sampler/utils.py
+++ ensemble-sampler/utils.py
@@ -50,3 +50,25 @@ class GPSTime(object):
         t.gpsNanoSeconds = self.ns
 
         return t
+
+def tail(f, n):
+    """Returns the last ``n`` lines of the file ``f``."""
+
+    if not (isinstance(f, file)):
+        with open(f, 'r') as inp:
+            return tail(f, n)
+
+    i = 0
+    lines = []
+    for l in f:
+        if i < n:
+            lines.append(l)
+            i = i+1
+        else:
+            lines[i%n] = l
+            i = i+1
+
+    if i < n:
+        raise ValueError('tail: file must have at least {0:d} lines'.format(n))
+
+    return lines[i%n:] + lines[:i%n]
