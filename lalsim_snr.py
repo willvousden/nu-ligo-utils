fixed f_low_restricted calculation in SNR script

lalsim_snr.py
@@ -27,7 +27,7 @@ class _vectorize_swig_psd_func(object):
             ret = ret.astype(float)
         return ret
 
-def get_inj_info(temp_amp_order, inj, event=0, ifos=['H1','L1','V1'], era='advanced', intended_flow=30):
+def get_inj_info(temp_amp_order, inj, event=0, ifos=['H1','L1','V1'], era='advanced', f_low=30.):
     noise_psd_funcs = {}
     if era == 'initial':
         for _ifos, _func in (
@@ -59,8 +59,11 @@ def get_inj_info(temp_amp_order, inj, event=0, ifos=['H1','L1','V1'], era='advan
     if temp_amp_order is None or temp_amp_order == -1:
         temp_amp_order = max_precessing_amp_pn_order if approx == lalsim.SpinTaylorT4 else max_nonprecessing_amp_pn_order
   
-    flow = event.f_lower
-    flow_hm = intended_flow / (1.+temp_amp_order)
+    f_low_inj = event.f_lower
+
+    # f_low is the lower frequency to be used for integration, thus the starting frequency of the 2,2 mode is
+    #  lower for waveforms with higher harmonics
+    f_low_restricted = f_low * 2 / float(temp_amp_order+2)
     f_isco = 1.0 / (6.0 * np.sqrt(6.0) * np.pi * (event.mass1+event.mass2) * lalsim.lal.LAL_MTSUN_SI)
   
     fhigh_fudgefactor = 1.1
@@ -69,7 +72,7 @@ def get_inj_info(temp_amp_order, inj, event=0, ifos=['H1','L1','V1'], era='advan
     while srate < nyquist: srate *= 2.
   
     seglen_fudgefactor = 1.1
-    chirptime =  seglen_fudgefactor * lalsim.SimInspiralTaylorF2ReducedSpinChirpTime(intended_flow, mass1, mass2, chi, phase_order)
+    chirptime =  seglen_fudgefactor * lalsim.SimInspiralTaylorF2ReducedSpinChirpTime(f_low_restricted, mass1, mass2, chi, phase_order)
   
     seglen = 1.0
     while seglen < chirptime: seglen *= 2.
@@ -84,7 +87,7 @@ def get_inj_info(temp_amp_order, inj, event=0, ifos=['H1','L1','V1'], era='advan
         mass1, mass2,
         event.spin1x, event.spin1y, event.spin1z,
         event.spin2x, event.spin2y, event.spin2z,
-        flow, 0,
+        f_low_inj, 0,
         event.distance * 1.0e6 * lalsim.lal.LAL_PC_SI,
         event.inclination,
         0, 0, None, None,
@@ -103,24 +106,23 @@ def get_inj_info(temp_amp_order, inj, event=0, ifos=['H1','L1','V1'], era='advan
         freqs = np.arange(h_tilde.f0, h_tilde.data.length*h_tilde.deltaF, h_tilde.deltaF)
         np.seterr(divide='ignore')
   
-        SNR = np.sum([np.power(h_tilde.data.data.real[j],2)/psd(freqs[j]) for j in xrange(int(flow/h_tilde.deltaF), h_tilde.data.length)])
-        SNR += np.sum([np.power(h_tilde.data.data.imag[j],2)/psd(freqs[j]) for j in xrange(int(flow/h_tilde.deltaF), h_tilde.data.length)])
+        SNR = np.sum([np.power(h_tilde.data.data.real[j],2)/psd(freqs[j]) for j in xrange(int(f_low_inj/h_tilde.deltaF), h_tilde.data.length)])
+        SNR += np.sum([np.power(h_tilde.data.data.imag[j],2)/psd(freqs[j]) for j in xrange(int(f_low_inj/h_tilde.deltaF), h_tilde.data.length)])
         SNR *= 4.*h_tilde.deltaF
         networkSNR+=SNR
 
     networkSNR = np.sqrt(networkSNR)
     networkSNR /= 2.  # Why!?
-    return networkSNR, srate, seglen, flow_hm
+    return networkSNR, srate, seglen, f_low_restricted
 
 
-def get_bns_info(intended_flow=30):
+def get_bns_info(f_low=30):
     # Nyquist for highest harmonic
     mass1 = mass2 = 1.4
     phase_order = max_phase_order
     amp_order = max_nonprecessing_amp_pn_order
   
-    flow = intended_flow
-    flow_hm = intended_flow / (1+amp_order)
+    f_low_restricted = f_low * 2 / float(amp_order+2)
     f_isco = 1.0 / (6.0 * np.sqrt(6.0) * np.pi * (mass1+mass2) * lalsim.lal.LAL_MTSUN_SI)
     fhigh_fudgefactor = 1.1
     nyquist = 2*(f_isco*fhigh_fudgefactor*(1+amp_order))
@@ -131,12 +133,12 @@ def get_bns_info(intended_flow=30):
     mass1 *= lalsim.lal.LAL_MSUN_SI
     mass2 *= lalsim.lal.LAL_MSUN_SI
     seglen_fudgefactor = 1.1
-    chirptime =  seglen_fudgefactor * lalsim.SimInspiralTaylorF2ReducedSpinChirpTime(flow, mass1, mass2, 0., phase_order)
+    chirptime =  seglen_fudgefactor * lalsim.SimInspiralTaylorF2ReducedSpinChirpTime(f_low, mass1, mass2, 0., phase_order)
   
     seglen = 1.0
     while seglen < chirptime: seglen *= 2.
   
-    return srate, seglen, flow_hm
+    return srate, seglen, f_low_restricted 
 
 
 if __name__ == '__main__':