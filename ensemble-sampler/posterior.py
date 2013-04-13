Added cython code for sum in time-phase marginalization, fixed normalization of log(L).

ensemble-sampler/posterior.py
@@ -524,6 +524,9 @@ class TimeMarginalizedPosterior(Posterior):
         dh = logaddexp_sum(dh_timeshifts)
         ll += dh
 
+        # Normalization for time integral
+        ll -= np.log(self.T)
+
         if self.malmquist_snr is not None:
             if len(hh_list) == 1:
                 hh2nd = hh_list[0]
@@ -545,7 +548,7 @@ class TimeMarginalizedPosterior(Posterior):
         params = to_time_marginalized_params(params)
         params_full = time_marginalized_params_to_params(params, time = 0.5*self.T)
 
-        return super(TimeMarginalizedPosterior, self).log_prior(params_full) - np.log(self.T)
+        return super(TimeMarginalizedPosterior, self).log_prior(params_full)
 
     def draw_prior(self, shape=(1,)):
         pfull = super(TimeMarginalizedPosterior, self).draw_prior(shape=shape)
@@ -610,8 +613,11 @@ class TimePhaseMarginalizedPosterior(Posterior):
             ll += -0.5*hh
 
         dh_timeshifts = 2.0*np.sqrt(dh_timeshifts_cos*dh_timeshifts_cos + dh_timeshifts_sin*dh_timeshifts_sin)
-        dh = logaddexp_sum(np.log(ss.ive(0, dh_timeshifts)) + dh_timeshifts)
-        ll += dh
+        dh = logaddexp_sum_bessel(ss.ive(0, dh_timeshifts), dh_timeshifts)
+        ll += dh 
+
+        # Normalization for time integral
+        ll -= np.log(self.T)
 
         if self.malmquist_snr is not None:
             if len(hh_list) == 1:
@@ -634,7 +640,7 @@ class TimePhaseMarginalizedPosterior(Posterior):
         params = to_time_phase_marginalized_params(params)
         params_full = time_phase_marginalized_params_to_params(params, time = 0.5*self.T, phi=np.pi/2.0)
 
-        return super(TimePhaseMarginalizedPosterior, self).log_prior(params_full) - np.log(self.T) - np.log(np.pi)
+        return super(TimePhaseMarginalizedPosterior, self).log_prior(params_full)
 
     def draw_prior(self, shape=(1,)):
         pfull = super(TimePhaseMarginalizedPosterior, self).draw_prior(shape=shape)

ensemble-sampler/run.py
@@ -39,13 +39,6 @@ class LogPrior(object):
     def __call__(self, params):
         return self.lnpost.log_prior(params)
 
-class ArgmaxLogLikelihoodPhiD(object):
-    def __init__(self, lnpost):
-        self.lnpost = lnpost
-
-    def __call__(self, params):
-        return self.lnpost.argmax_log_likelihood_phid(params)
-
 def reset_files(ntemps):
     for i in range(ntemps):
         with open('chain.{0:02d}.dat'.format(i), 'r') as inp:
@@ -62,32 +55,6 @@ def reset_files(ntemps):
             header = inp.readline()
         with open('chain.{0:02d}.lnpost.dat'.format(i), 'w') as out:
             out.write(header)
-
-def maximize_phase_distance(par, lnpost, nthreads=1):
-    """Returns parameters maximized over phase and distance.
-
-    :param par: A float array of shape ``(..., nparams)``.
-    
-    :param lnpost: A :class:pos.TimeMarginalizedPosterior object.
-
-    :param nthreads: Number of threads to use for maximization
-    computation
-
-    """
-
-    if nthreads > 1:
-        pool = multi.Pool(nthreads)
-        m = lambda f, l: pool.map(f, l, chunksize=len(l)/(nthreads+2))
-    else:
-        m = map
-
-    argmax = ArgmaxLogLikelihoodPhiD(lnpost)
-
-    shape = par.shape
-
-    best_params = np.array(m(argmax, par.reshape((-1, params.nparams_time_marginalized))))
-
-    return best_params.reshape(shape)
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='run an ensemble MCMC analysis of a GW event')
@@ -140,14 +107,15 @@ if __name__ == '__main__':
     if args.inj_params is not None:
         inj_params = np.loadtxt(args.inj_params)
 
-    lnposterior = pos.TimeMarginalizedPosterior(time_data=time_data,
-                                                inj_params=inj_params, T=args.seglen,
-                                                time_offset=gps_start,
-                                                srate=args.srate,
-                                                malmquist_snr=args.malmquist_snr,
-                                                mmin=args.mmin, mmax=args.mmax,
-                                                dmax=args.dmax,
-                                                dataseed=args.dataseed)
+    lnposterior = \
+            pos.TimePhaseMarginalizedPosterior(time_data=time_data,
+                                               inj_params=inj_params, T=args.seglen,
+                                               time_offset=gps_start,
+                                               srate=args.srate,
+                                               malmquist_snr=args.malmquist_snr,
+                                               mmin=args.mmin, mmax=args.mmax,
+                                               dmax=args.dmax,
+                                               dataseed=args.dataseed)
 
     if args.Tstep is None:
         ndim = params.nparams
@@ -164,7 +132,7 @@ if __name__ == '__main__':
     NTs = Ts.shape[0]    
 
     # Set up initial configuration
-    nparams = params.nparams_time_marginalized
+    nparams = params.nparams_time_phase_marginalized
     p0 = np.zeros((NTs, args.nwalkers, nparams))
     means = []
     if args.restart:
@@ -175,8 +143,6 @@ if __name__ == '__main__':
     else:
         for i in range(NTs):
             p0[i,:,:] = lnposterior.draw_prior(shape=(args.nwalkers,)).view(float).reshape((args.nwalkers, nparams))
-
-        p0 = maximize_phase_distance(p0, lnposterior, nthreads=args.nthreads)
 
     sampler = emcee.PTSampler(NTs, args.nwalkers, nparams, LogLikelihood(lnposterior), 
                               LogPrior(lnposterior), threads = args.nthreads, 
@@ -243,7 +209,6 @@ if __name__ == '__main__':
                 for j in range(p0.shape[1]):
                     if lnposterior.log_prior(p0[i,j,:]) == float('-inf'):
                         p0[i,j,:] = lnposterior.draw_prior()
-            p0 = maximize_phase_distance(p0, lnposterior, nthreads=args.nthreads)
 
             # Make sure to store the best point in the sample, too!
             p0[0,0,:] = pbest