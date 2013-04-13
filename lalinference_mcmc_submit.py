f3b42bc7ad9f4e79fd5166a1e4742f7beacfb093
Benjamin Farr <bfarr@u.northwestern.edu>
2013-04-13 12:04:43 -0600

    now checks for fixed params and psd arguments

--- lalinference_mcmc_submit.py
+++ lalinference_mcmc_submit.py
@@ -21,6 +21,13 @@ def exists(filename):
         print "Warning: {} not found.".format(rc)
         return False
 
+def check_for_arg_substring(arg_str, args):
+    arg_check = ['-{}'.format(arg_str) in arg for arg in args]
+    return True if True in arg_check else False
+
+def num_of_arg_substring(arg_str, args):
+    arg_check = ['-fix' in arg for arg in li_args]
+    return sum(arg_check)
 
 parser = argparse.ArgumentParser(description='Generate a submit file for lalinference_mcmc on grail.')
 
@@ -42,7 +49,7 @@ msub.add_argument('--name', default='submit',
         help='Name of submit file (default=submit).')
 msub.add_argument('--walltime', default='2:00:00:00',
         help='Walltime for job (default=2:00:00:00).')
-msub.add_argument('--nPar', default=None,
+msub.add_argument('--nPar', default=None, type=int,
         help='Number of dimensions for MCMC.  Defaults for common templates are set, assuming no fixed parameters or PSD fitting.')
 
 env.add_argument('--branch', default='master',
@@ -51,14 +58,14 @@ env.add_argument('--rc', action='append',
         help='Specify direct path to rc files to be sourced (e.g. lscsoftrc).  /projects/p20128/non-lsc/lscsoft-user-env.sh is added by default.')
 li_mcmc.add_argument('--era', required=True, default='advanced',
         help='Era ("initial" or "advanced") of detector PSD for SNR calculation.  If no cache arguments given, this will add the appropriate analytical PSD arguments to the submit file.')
-li_mcmc.add_argument('--ifo', default=['H1','L1','V1'], action='append',
+li_mcmc.add_argument('--ifo', nargs='+',
         help='IFOs for the analysis.')
 li_mcmc.add_argument('--inj',
         help='Injection XML file.')
 li_mcmc.add_argument('--event', type=int,
         help='Event number in XML to inject.')
-li_mcmc.add_argument('--approx', default=None,
-        help='Specify a template approximant (default=max available for APPROX).')
+li_mcmc.add_argument('--approx', default='SpinTaylorT4',
+        help='Specify a template approximant (default SpinTaylorT4).')
 li_mcmc.add_argument('--ampOrder', default=None,
         help='Specify amplitude order of template.')
 li_mcmc.add_argument('--flow', default=40., type=float,
@@ -67,7 +74,11 @@ li_mcmc.add_argument('--srate', default=None, type=float,
         help='Sampling rate of the waveform.  If not provided and an injection is peformed, it is set to be sufficient for the signal being injected.  If no injection, it defaults to a sufficient value for a 1.4-1.4 binary coalescence (expensive!).')
 li_mcmc.add_argument('--seglen', default=None, type=float,
         help='Length of data segment used for likelihood compuatation.  Same default behavior as "--srate".')
-li_mcmc.add_argument('--tempLadderBottomUp', default=False, action='store_true',
+li_mcmc.add_argument('--psdlength', default=None, type=float,
+        help='Length of data segment to use for PSD estimation.  Defaults to 32*seglen.')
+li_mcmc.add_argument('--psdstart', default=None, type=float,
+        help='GPS time to start PSD calculation.  Defaults to trigtime - psdlength - seglen')
+li_mcmc.add_argument('--tempLadderTopDown', default=False, action='store_true',
         help='Build the temperature ladder from the bottom up, using an analytic prescription for the spacing that should ensure communication between chains.  Sets the number of cores so that the hottest temperature should be sampling the prior.')
 li_mcmc.add_argument('--trigSNR', default=None, type=float,
         help='SNR of the trigger (calculated automatically if injecting).')
@@ -114,17 +125,33 @@ elif args.era == 'advanced':
 
 
 # Check if caches specified in extra arguments
-cache_check = ['-cache' in arg for arg in li_args]
-caches_specified = True if True in cache_check else False
+caches_specified = check_for_arg_substring('cache', li_args)
 
-# Default number of parameters for a few common templates.  Assume no fixed
-#  parameters and no PSD fitting
+# Check for fixed params
+n_fixed_params = num_of_arg_substring('fix', li_args)
+
+# Check for spin aligned flag
+spin_aligned = check_for_arg_substring('spinAligned', li_args)
+
+# Check for no-spin flag
+no_spin = check_for_arg_substring('noSpin', li_args) or check_for_arg_substring('disable-spin', li_args)
+
+# Default number of parameters for a few common templates, assuming no PSD fitting.
+#  Some values are modified if spin flags are present.  This copies the
+#  behavior of LALInference for these cases.
 nPars = {lalsim.SpinTaylorT4:15,
-        lalsim.IMRPhenomB:11,
-        lalsim.TaylorF2:9, lalsim.TaylorF2RedSpin:11,
+        lalsim.IMRPhenomB:9,
+        lalsim.TaylorF2:9, lalsim.TaylorF2RedSpin:9,
         lalsim.TaylorT1:9, lalsim.TaylorT2:9, lalsim.TaylorT3:9, lalsim.TaylorT4:9,
         lalsim.EOB:9, lalsim.EOBNR:9, lalsim.EOBNRv2:9, lalsim.EOBNRv2HM:9,
-        lalsim.SEOBNRv1:11}
+        lalsim.SEOBNRv1:9}
+
+if spin_aligned or no_spin:
+    nModifiedPar = 9 if no_spin else 11
+    nPars[lalsim.SpinTaylorT4] = nModifiedPar
+    nPars[lalsim.IMRPhenomB] = nModifiedPar
+    nPars[lalsim.TaylorF2RedSpin] = nModifiedPar
+    nPars[lalsim.SEOBNRv1] = nModifiedPar
 
 # Target likelihood value "sampled" by the hottest chain.  Same as in lalinference_mcmc.
 target_hot_like = 15.
@@ -158,6 +185,8 @@ rcs[:] = [rc for rc in rcs if exists(rc)]
 modules = ['python','mpi/openmpi-1.6.3-intel2013.2']
 
 # Determine sampling rate, segment length, and SNR (--trigSNR takes precedence).
+approx = lalsim.GetApproximantFromString(args.approx)
+
 if args.ampOrder is None:
     amp_order = None
 else:
@@ -167,11 +196,7 @@ else:
         amp_order = lalsim.GetOrderFromString(args.ampOrder)
 
 SNR = None
-if args.trigSNR:
-    calcSNR=False
-else:
-    calcSNR = True
-
+calcSNR = False if args.trigSNR else True
 
 if args.inj and args.event is not None:
     SNR, srate, seglen, flow = get_inj_info(amp_order, args.inj, args.event, args.ifo, args.era, args.flow, calcSNR)
@@ -181,6 +206,8 @@ else:
 
 if args.trigSNR:
     SNR = args.trigSNR
+else:
+    print "Network SNR: {}".format(SNR)
 
 if args.srate:
     srate = args.srate
@@ -191,9 +218,14 @@ if args.seglen:
 if srate > srate_max:
     srate = srate_max
 
+# Determine trigger time
+trigtime = None
+if args.inj and args.event is not None:
+    event = SimInspiralUtils.ReadSimInspiralFromFiles([args.inj])[args.event]
+    trigtime = event.geocent_end_time + 1e-9*event.geocent_end_time_ns
 
 # Ladder spacing flat in the log.  Analytic delta
-if args.tempLadderBottomUp:
+if not args.tempLadderTopDown:
     li_args.append('--tempLadderBottomUp')
 
     # Determine maximum temperature
@@ -210,16 +242,13 @@ if args.tempLadderBottomUp:
     if args.nPar:
         nPar = args.nPar
     else:
-        if args.approx:
-            approx = lalsim.GetApproximantFromString(args.approx)
-        else:
-            print "No approximant or number of parameters given. Defaulting to conservative temperature spacing, with nPar=15."
-            approx = lalsim.SpinTaylorT4
-
         try:
             nPar = nPars[approx]
         except KeyError:
             raise(KeyError, "No default value for given approx.  Specity explicitly with the --nPar argument.")
+        nPar -= n_fixed_params
+
+        print "nPar: {}".format(nPar)
 
     temp_delta = 1 + np.sqrt(2./nPar)
 
@@ -239,8 +268,17 @@ flow_args = ['--{}-flow {:g}'.format(ifo, flow) for ifo in ifos]
 if not caches_specified:
     cache_args = ['--{}-cache {}'.format(ifo, noise_psd_caches[ifo]) for ifo in ifos]
 
-if on_quest:
-    with open(submitFilePath,'w') as outfile:
+# PSD-related args
+psd_args = ''
+ 
+psdlength = args.psdlength if args.psdlength else 32*seglen
+psd_args += '--psdlength {}'.format(psdlength)
+
+psdstart = args.psdstart if args.psdstart else trigtime-psdlength-seglen
+psd_args += ' --psdstart {}'.format(psdstart)
+
+with open(submitFilePath,'w') as outfile:
+    if on_quest:
         # MSUB directives
         outfile.write('#!/bin/bash\n')
         outfile.write('#MSUB -A {}\n'.format(args.alloc))
@@ -256,7 +294,7 @@ if on_quest:
             outfile.write('#MSUB -N {}\n'.format(args.jobName))
 
         outfile.write('#MSUB -j oe\n')
-        outfile.write('#MOAB -d {}\n'.format(args.dir))
+        outfile.write('#MSUB -d {}\n'.format(args.dir))
         outfile.write('\n')
 
         # Ensure core dump on failure
@@ -266,18 +304,28 @@ if on_quest:
         outfile.writelines(['module load {}\n'.format(module) for module in modules])
         outfile.write('\n')
 
-        # Load LALSuite environment
-        outfile.writelines(['source {}\n'.format(rc) for rc in rcs])
-        outfile.write('\n')
+    # Load LALSuite environment
+    outfile.writelines(['source {}\n'.format(rc) for rc in rcs])
+    outfile.write('\n')
 
-        # lalinference_mcmc command line
-        outfile.write('mpirun lalinference_mcmc\\\n')
-        outfile.write('  {}\\\n'.format(' '.join(ifo_args)))
-        outfile.write('  {}\\\n'.format(' '.join(flow_args)))
+    # lalinference_mcmc command line
+    outfile.write('mpirun lalinference_mcmc\\\n')
+    outfile.write('  {}\\\n'.format(' '.join(ifo_args)))
+    if not caches_specified:
+        outfile.write('  {}\\\n'.format(' '.join(cache_args)))
+    outfile.write('  {}\\\n'.format(' '.join(flow_args)))
 
-        if not caches_specified:
-            outfile.write('  {}\\\n'.format(' '.join(cache_args)))
+    if args.inj and args.event is not None:
+        outfile.write('  --inj {} --event {}\\\n'.format(args.inj, args.event))
 
-        outfile.write('  --srate {:g}\\\n'.format(srate))
-        outfile.write('  --seglen {:g}\\\n'.format(seglen))
-        outfile.write('  {}'.format(' '.join(li_args)))
+    if trigtime is not None:
+        outfile.write('  --trigtime {}\\\n'.format(trigtime))
+
+    outfile.write('  {}\\\n'.format(psd_args))
+    outfile.write('  --srate {:g}\\\n'.format(srate))
+    outfile.write('  --seglen {:g}\\\n'.format(seglen))
+    outfile.write('  --approx {}\\\n'.format(args.approx))
+    if amp_order is not None:
+        outfile.write('  --ampOrder {}\\\n'.format(amp_order))
+
+    outfile.write('  {}'.format(' '.join(li_args)))
