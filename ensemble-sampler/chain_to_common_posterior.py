d36c6c9e3d7199217432e25f33eed9af19679d20
Will Meierjurgen Farr <wmfarr@gmail.com>
2013-06-18 14:54:42 -0500

    Added burnin option to chain_to_common

--- ensemble-sampler/chain_to_common_posterior.py
+++ ensemble-sampler/chain_to_common_posterior.py
@@ -10,6 +10,9 @@ if __name__ == '__main__':
 
     parser.add_argument('--chain', metavar='FILE', required=True, help='chain file')
     parser.add_argument('--output', metavar='FILE', default='posterior_samples.dat', help='output file')
+    parser.add_argument('--nwalkers', metavar='N', type=int, help='number of walkers')
+    parser.add_argument('--fburnin', metavar='F', type=float, help='fraction of samples to discard as burnin')
+    parser.add_argument('--decorrelated', action='store_true', help='output only decorrelated samples')
 
     args = parser.parse_args()
 
@@ -26,6 +29,23 @@ if __name__ == '__main__':
     logls = np.loadtxt(logl_file).reshape((-1,1))
     logps = np.loadtxt(logp_file).reshape((-1,1))
 
+    if args.fburnin is not None:
+        assert args.nwalkers is not None, 'need to specify number of walkers for burnin'
+        chain = chain.reshape((-1, args.nwalkers, chain.shape[1]))
+        logls = logls.reshape((-1, args.nwalkers))
+        logps = logps.reshape((-1, args.nwalkers))
+
+        nchain = chain.shape[0]
+        istart = int(round(nchain*args.fburnin))
+
+        chain = chain[istart:, ...]
+        logls = logls[istart:,:]
+        logps = logps[istart:,:]
+
+        chain = chain.reshape((-1, chain.shape[2]))
+        logls = logls.reshape((-1, 1))
+        logps = logps.reshape((-1, 1))
+    
     with open(args.output, 'w') as out:
         out.write(' '.join(chain_header + ['logl', 'logpost']) + '\n')
         np.savetxt(out, np.column_stack((chain, logls, logps)))
